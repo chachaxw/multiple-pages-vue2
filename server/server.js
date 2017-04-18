c0c84ac48b1fb50f6a3855a17f90639a92d8cf9d
chacha <867571123@qq.com>
2017-04-18 18:05:08 +0800

    update project

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
+
 const Koa = require('koa');
+const fs = require('fs');
+const path = require('path');
+const static = require('koa-static');
+
 const argv = require('yargs').argv;
 const controller = require('./controller');
 const router = require('koa-router')();
@@ -13,6 +18,8 @@ const app = new Koa();
 app.use(cors());
 
 app.use(controller(router));
+
+app.use(static(path.resolve(__dirname, 'dist')));
 
 app.on('error', (err, ctx) => {
 	console.log('GLOBAL-ERROR:');
